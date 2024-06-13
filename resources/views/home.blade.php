8c6e09afd3b1df91ab71e0ed1716101b9d3aec79
gnaaal <segalangnya.rd@gmail.com>
2024-06-13 10:34:00 +0700

    Frontend

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -44,11 +44,12 @@
         </nav>
 
         <!-- Header-->
-        <header class="bg-dark py-5">
+        <header class="py-5">
             <div class="container px-4 px-lg-5 my-5">
-                <div class="text-center text-white">
-                    <h1 class="display-4 fw-bolder">Shop in style</h1>
-                    <p class="lead fw-normal text-white-50 mb-0">With this shop hompeage template</p>
+                <div class="text-star text-white">
+                    <h1 class="display-4 fw-bolder">Grab Upto 50% Off<br>On Selected Items</h1>
+                    <p class="lead fw-normal text-white-50 mb-0">Limited Time Offers</p>
+                    <button type="button" class="btn btn-dark">Dark</button>
                 </div>
             </div>
         </header>
