6b7334845f034bb8b386b6b3ff57742383466f4a
Deppn <devenyuu13@gmail.com>
2024-06-13 12:43:36 +0700

    Login page

--- app/Http/Controllers/homeController.php
+++ app/Http/Controllers/homeController.php
@@ -7,5 +7,13 @@ use Illuminate\Http\Request;
 
 class homeController extends Controller
 {
-    //
+    public function index(){
+        $users = User::all();
+        return view('users.index',['users'=> $users]);
+    }
+
+    public function show($id) {
+        $user = User::findOrFail($id);
+        return view('users.detail',['user'=>$user]);
+    }
 }
