8bfec97e9ab033514b14f02d16aba2b7ff9cd071
Deppn <devenyuu13@gmail.com>
2024-06-26 19:15:14 +0700

    update user

---
+++ app/Http/Controllers/User/ProfileController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers\User;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class ProfileController extends Controller
+{
+    public function edit()
+    {
+        return view('user.profile.edit');
+    }
+
+    public function update(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+        ]);
+
+        $user = Auth::user();
+        $user->name = $request->input('name');
+        $user->save();
+
+        return redirect()->route('profile.edit')->with('success', 'Profile updated successfully.');
+    }
+}
+
