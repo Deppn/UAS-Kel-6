df70d83f57f42507b661c3caadee1ad9bed8188e
richielagito <richielagito1@gmail.com>
2024-06-27 20:13:06 +0700

    fungsi menampilkan produk ke ui

---
+++ resources/views/home/index.blade.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Ube</title>
+        <!-- Favicon-->
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+        <!-- Bootstrap icons-->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="css/styles.css" rel="stylesheet" />
+        <!-- Google fonts -->
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Pacifico&family=Playwrite+DE+SAS:wght@100..400&family=Righteous&display=swap" rel="stylesheet">
+    </head>
+    <body>
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-light">
+            <div class="container px-0">
+                <a class="navbar-brand me-auto fs-1 ube" href="#!">Ube</a>
+                <div class="d-flex">
+                    <ul class="navbar-nav mb-2 mb-lg-0 ms-lg-4">
+                        <input class="form-control me-2 icon-rtl" type="search" value placeholder="Search" aria-label="Search">
+                        <div class="d-flex align-items-center ms-3">
+                            <i class="bi bi-person-fill"></i>
+                            <li class="nav-item"><a class="nav-link" href="/login">Login</a></li>
+                            <li class="nav-item"><a class="nav-link" href="/register">Register</a></li>
+                        </div>
+                    </ul>
+                    <form class="d-inline-flex ms-3" action="/cart" method="get">
+                        <button class="btn btn-outline-purple px-3" type="submit">
+                            <i class="bi-cart-fill me-1"></i>
+                            Cart
+                        </button>
+                    </form>
+
+                </div>
+            </div>
+        </nav>
+
+        <!-- Header-->
+        <header class="mt-2">
+            <div class="container d-flex justify-content-around align-items-center">
+                <div class="text-star text-white">
+                    <h1 class="display-4 fw-bolder">Grab Upto 50% Off<br>On Selected Items</h1>
+                    <p class="lead fw-normal fs-5 text-white-50 mb-1">*Limited Time Offers Only</p>
+                    <a href="#product"><button type="button" class="btn btn-purple text-white px-4 py-3 mt-4">SHOP NOW</button></a>
+                </div>
+                <img class="position-relative headerphoto" style="height: 430px"src="assets/headerphoto.png" alt="headerphoto">
+            </div>
+        </header>
+
+        <!-- Products -->
+        @include('home.product')
+        
+        <!-- Footer-->
+        <footer class="py-5 bg-purple">
+            <div class="container"><p class="m-0 text-center text-white">Copyright &copy; Ube 2024</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,42 +1,30 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
-use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\UserController;
 use App\Http\Controllers\homeController;
-use App\Http\Controllers\Admin\AdminController;
 
-Route::get('/', function () {
-    return view('home');
-});
+// Route::get('/', function () {
+//     return view('home.index');
+// });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
+Route::get('/', [homeController::class, 'index']);
 
-Route::middleware(['auth'])->group(function () {
-    Route::controller(DashboardController::class)->group(function () {
-        Route::get('/admin/dashboard', 'index');
-    });
-});
-Route::get('/view_category', [AdminController::class, 'view_category'])->name('admin.category');
-Route::post('add_category', [AdminController::class, 'add_category'])->name('add_category');
-Route::get('delete_category/{id}', [AdminController::class, 'delete_category'])->name('delete_category');
-Route::get('edit_category/{id}', [AdminController::class, 'edit_category'])->name('edit_category');
-Route::post('update_category/{id}', [AdminController::class, 'update_category'])->name('update_category');
-Route::get('add_product', [AdminController::class, 'add_product'])->name('add_product');
-Route::post('upload_product', [AdminController::class, 'upload_product'])->name('upload_product');
-Route::get('view_product', [AdminController::class, 'view_product'])->name('view_product');
+// Route::get('/dashboard', [homeController::class, 'login_home'])->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
+use App\Http\Controllers\MovieController;
 
-Route::get('/cart', [AdminController::class, 'Cart']);
-Route::post('add-to-cart', [AdminController::class, 'addToCart'])->name('products.addToCart');
-Route::delete('/delete-cart-item', [AdminController::class, 'deleteItem'])->name('delete.cart.item');
+Route::get('/shopping', [ProductController::class, 'index']);   
+Route::get('/cart', [ProductController::class, 'Cart']);
+Route::post('add-to-cart', [ProductController::class, 'addToCart'])->name('products.addToCart');
+Route::delete('/delete-cart-item', [ProductController::class, 'deleteItem'])->name('delete.cart.item');
 
-
-require __DIR__.'/auth.php';
+Route::get('login', [AuthController::class, 'index'])->name('login');
+Route::post('post-login', [AuthController::class, 'postLogin'])->name('login.post'); 
+Route::get('registration', [AuthController::class, 'registration'])->name('register');
+Route::post('post-registration', [AuthController::class, 'postRegistration'])->name('register.post'); 
+Route::get('dashboard', [AuthController::class, 'dashboard']); 
+Route::post('logout', [AuthController::class, 'logout'])->name('logout');
+Route::resource('products', productController::class);
