d80decb81a76100b2d5850962c204dbda2472703
jalssonn <jallson30191609@gmail.com>
2024-06-27 15:24:26 +0700

    product

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -60,117 +60,31 @@ class AdminController extends Controller
         return redirect('/view_category');
     }
 
-    public function index()
+    public function add_product()
     {
-        $products = Product::all();
-        return view('products.index', compact('products'));
+        $category = category::all();
+        return view ('admin.add_product',compact('category'));
     }
-
-    public function create()
+    public function upload_product(request $request)
     {
-        return view('products.create');
-    }
+        $data = new product;
 
-    public function store(Request $request)
-    {
-        $request->validate([
-            'image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
-            'name' => 'required',
-            'description' => 'required',
-            'price' => 'required|numeric',
-            'stock' => 'required|integer',
-        ]);
+        $data-> title=$request->title;
+        $data-> description=$request->description;
+        $data-> price=$request->price;
+        $data-> quantity=$request->qty;
+        $data-> category=$request->category;
+        $image=$request->image;
+        if($image)
+        {
+            $imagename = time().'.'.$image->getClientOriginalExtension();
+            
+            $request->image->move('products',$imagename);
 
-        Product::create($request->all());
-
-        return redirect()->route('products.index')->with('success', 'Product created successfully.');
-    }
-
-    public function show(Product $product)
-    {
-        return view('products.show', compact('product'));
-    }
-
-    public function edit(Product $product)
-    {
-        return view('products.edit', compact('product'));
-    }
-
-    public function update(Request $request, Product $product)
-    {
-        $request->validate([
-            'image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
-            'name' => 'required',
-            'description' => 'required',
-            'price' => 'required|numeric',
-            'stock' => 'required|integer',
-        ]);
-
-        $product->update($request->all());
-
-        return redirect()->route('products.index')->with('success', 'Product updated successfully.');
-    }
-
-    public function destroy(Product $product)
-    {
-        $product->delete();
-
-        return redirect()->route('products.index')->with('success', 'Product deleted successfully.');
-    }
-
-    public function Cart()
-    {
-        return view('cart');
-    }
-    public function addToCart(Request $request)
-    {
-        $productId = $request->input('product_id');
-        $quantity = $request->input('quantity', 1);
-        $cartItemId = $request->input('cart_item_id');
-
-        $product = Product::find($productId);
-
-        if (!$product) {
-            return response()->json(['error' => 'Product not found'], 404);
+            $data->image = $imagename;
         }
-
-        $cart = session()->get('cart', []);
-
-        if (isset($cart[$productId])) {
-            // Update quantity if product is already in the cart
-            $cart[$productId]['quantity'] += $quantity;
-        } else {
-            // Add new item to the cart
-            $cart[$productId] = [
-                'id' => $product->id,
-                'name' => $product->name,
-                'price' => $product->price,
-                'quantity' => $quantity,
-                'poster' => $product->poster,
-            ];
-        }
-
-        session()->put('cart', $cart);
-
-        // Calculate the total quantity
-        $totalQuantity = 0;
-        foreach ($cart as $item) {
-            $totalQuantity += $item['quantity'];
-        }
-        return response()->json(['message' => 'Cart updated', 'cartCount' => $totalQuantity], 200);
-    }
-
-    
-    public function deleteItem(Request $request)
-    {
-        if($request->id) {
-            $cart = session()->get('cart');
-            if(isset($cart[$request->id])) {
-                unset($cart[$request->id]);
-                session()->put('cart', $cart);
-            }
-            session()->flash('success', 'Movie successfully deleted.');
-        }
+        $data->save();
+        return redirect()->back();
     }
 
 }
