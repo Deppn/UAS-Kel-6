bff8f959e4fed475d2a5257c8df5815c9ade03fb
gnaaal <segalangnya.rd@gmail.com>
2024-06-27 17:57:21 +0700

    fix little details

--- resources/views/admin/sidebar.blade.php
+++ resources/views/admin/sidebar.blade.php
@@ -5,16 +5,16 @@
         <div class="sidebar-header d-flex align-items-center">
           <div class="avatar"><img src="{{asset('admincss/img/avatar-6.jpg')}}" alt="..." class="img-fluid rounded-circle"></div>
           <div class="title">
-            <h1 class="h5">Mark Stephen</h1>
-            <p>Web Designer</p>
+            <h1 class="h5">EL Gatoo</h1>
+            <p>Rookie Designer</p>
           </div>
         </div>
         <!-- Sidebar Navidation Menus--><span class="heading">Main</span>
         <ul class="list-unstyled">
                 <li class="active"><a href="index.html"> <i class="icon-home"></i>Home </a></li>
-                <li><a href="{{url('view_category')}}"> <i class="icon-grid"></i>category </a></li>
-                <li><a href="{{url('add_product')}}"> <i class="icon-grid"></i>product </a></li>
-                <li><a href="{{url('view_product')}}"> <i class="icon-grid"></i>view_product </a></li>
+                <li><a href="{{url('view_category')}}"> <i class="icon-grid"></i>Category </a></li>
+                <li><a href="{{url('add_product')}}"> <i class="icon-grid"></i>Product </a></li>
+                <li><a href="{{url('view_product')}}"> <i class="icon-grid"></i>View Product </a></li>
                   </ul>
                 </li>
               
